90562047c7de3f3c2a01d070144895e3f0926686
matheusbwv <matheus.wenes@gmail.com>
2025-03-28 11:43:45 -0300

    fix:authControllers

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    return view('test');
+});
+
+Route::get('/welcome', function () {
     return view('welcome');
 });
+
+Route::prefix('auth')->controller(AuthController::class)->group(function () {
+    Route::post('/', 'store')->name('auth.store');
+});
+
