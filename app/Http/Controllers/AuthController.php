8f03b9465a318e0fd3fe6d802c037f7880073e54
matheusbwv <matheus.wenes@gmail.com>
2025-03-28 09:28:58 -0300

    add:AuthController para login

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
+
+class AuthController extends Controller
+{
+    public function register(Request $request){
+
+        $request->validate([
+            'email' => 'required|string|max:255|unique:users',
+            'password' => 'required|string|min:6|confirmed',
+        ]);
+
+        $user = User::create([
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return redirect("/");
+        // return response()->json(['user' => $user], 201);
+    }
+
+    public function login(Request $request){
+        $request-> validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        if (!Auth::attempt($request->only('email', 'password'))) {
+        return response()->json(['message' => 'Credenciais inválidas'], 401);
+        }
+
+        $user = Auth::user();
+        // $token = $user -> createToken('auth_tocken')->plainTexttoken;
+
+        // return response()->json(['token' => $token, 'user' => $user]);
+    }
+
+    // Logout de usuário
+    public function logout(Request $request)
+    {
+        $request->user()->tokens()->delete();
+        return response()->json(['message' => 'Logout realizado com sucesso']);
+    }
+
+}
+
