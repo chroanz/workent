84bc4d0484a4b1654c3d9d88c9e1da47f45fef5f
matheusbwv <matheus.wenes@gmail.com>
2025-03-31 00:38:17 -0300

    fix:login e registro - routes adicionadas para registro e login

--- resources/views/pages/login.blade.php
+++ resources/views/pages/login.blade.php
@@ -3,24 +3,24 @@
 @section("content")
 <div class="bg-primary-color h-100 w-100 d-flex">
     <div class="d-flex align-items-center justify-content-center form-container">
-        <form action="{{ route('auth.store') }}" method="POST" class="bg-secondary-color form">
+        <form action="{{ route('auth.login') }}" method="POST" class="bg-secondary-color form">
             @csrf
             <h2 class="title-var">Entrar na conta</h2>
             <div class="d-flex flex-column w-75">
                 <label for="" class="label-var">Email</label>
-                <input type="email" class="input-var">
+                <input type="email" class="input-var" name="email">
             </div>
 
             <div class="d-flex flex-column w-75">
                 <label for="" class="label-var">Senha</label>
-                <input type="password" class="input-var">
+                <input type="password" class="input-var" name="password">
             </div>
             <div class="d-flex w-75 justify-content-end">
                 <a href="">Esqueci a senha</a>
             </div>
             <div class="d-flex flex-column w-75 gap-3">
                 <button type="submit" class="btn-var-primary">Entrar</button>
-                <button type="button" class="link-var-primary">Criar uma conta</button> 
+                <button type="button" class="link-var-primary">Criar uma conta</button>
             </div>
         </form>
     </div>
