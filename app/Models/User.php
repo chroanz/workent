3a474f06f12c0f6fc12581f1ece9cc00481b568a
giullian <giullianbernard2013@gmail.com>
2025-04-03 21:35:48 -0300

    client controller user client migration

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -37,8 +38,8 @@ class User extends Authenticatable
      * @return array<string, string>
      */
 
-     public function client()
+     public function client():HasOne
      {
-
+        return $this->hasOne(Client::class);
      }
 }
