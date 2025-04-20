025e70fec5782c3b686a8ff2164fe8ce1e8823d0
Natanael Felix <f.natanael.felix.b@gmail.com>
2025-04-20 11:48:09 -0300

    Fix: Ajusta ultimo merge
    Isso também remove comentários desnecessários

--- app/Http/Controllers/EvaluationController.php
+++ app/Http/Controllers/EvaluationController.php
@@ -7,66 +7,18 @@ use Illuminate\Http\Request;
 
 class EvaluationController extends Controller
 {
-    public function index()
-    {
-        // Deve retornar uma view de listagem um objeto nesse modelo para ser renderizado
-        // return view('evaluation.index',[
-        //     'evaluations' => Evaluation::all()
-        // ]);
-    }
-
     public function create()
     {
-        // Deve retornar uma view de criação um objeto nesse modelo para ser renderizado
         return view('pages/evaluation/create');
     }
 
     public function store(Request $request)
     {
-        // Deve receber os dados do formulário do método acima e criar um novo objeto nesse modelo
         $formFields = $request->validate([
             'comment' => 'string|max:255',
             'stars' => 'required|integer|min:1|max:5',
         ]);
         Evaluation::create($formFields);
-        // Ao finalizar a criação, deve redirecionar para a uma página adequada
         return redirect('/');
     }
-
-    public function show(Evaluation $evaluation)
-    {
-        // Deve retornar uma view de exibição de um objeto nesse modelo
-        // return view('evaluation.show', [
-        //     'evaluation' => $evaluation
-        // ]);
-    }
-
-    public function edit(Evaluation $evaluation)
-    {
-        // Deve retornar uma view de edição de um objeto nesse modelo
-        // return view('evaluation.edit', [
-        //     'evaluation' => $evaluation
-        // ]);
-    }
-
-    public function update(Request $request, Evaluation $evaluation)
-    {
-        // Deve receber os dados do formulário do método acima e atualizar o objeto nesse modelo
-        // $formFields = $request->validate([
-        //     'comment' => 'string|max:255',
-        //     'stars' => 'required|integer|min:1|max:5',
-        // ]);
-        // $evaluation->update($formFields);
-        // Ao finalizar a atualização, deve redirecionar para a uma página adequada
-        // return redirect('/');
-    }
-
-    public function destroy(Evaluation $evaluation)
-    {
-        // Deve deletar o objeto nesse modelo
-        // $evaluation->delete();
-        // return redirect('/');
-    }
-
-    // Abaixo do destroy, você pode adicionar métodos adicionais que fogem do padrão do Laravel
 }
--- resources/views/pages/profile/edit.blade.php
+++ resources/views/pages/profile/edit.blade.php
@@ -3,29 +3,40 @@
 @section('content')
   @include('components.header')
 
-  <form action="{{ route('profile.update') }}" method="POST"
-    enctype="multipart/form-data">
-    @csrf
-    @method('PUT')
-    <div>
-      <label for="name">Nome</label>
-      <input name="name" type="text" value="{{ $client->name }}" required>
-    </div>
+  <main class="perfil-container">
+    <section class="perfil-info">
+      <div class="avatar-nome">
+        <div class="avatar">
+          <img src="{{ asset('images/avatar-icon.svg') }}" alt="Avatar" />
+        </div>
+        <h2>{{ $client->name }}</h2>
+      </div>
 
-    <div>
-      <label for="birthday">Data de nascimento</label>
-      <input type="date" name="birthday" value="{{ $client->birthday }}"
-        required>
-    </div>
+      <form class="dados" action="{{ route('profile.update') }}" method="POST"
+        enctype="multipart/form-data">
+        @csrf
+        @method('PUT')
+        <label>Nome completo</label>
+        <input name="name" type="text" value="{{ $client->name }}"
+          required />
 
-    <div>
-      <label for="address">Endereço</label>
-      <textarea id="address" name="address">{{ $client->address }}</textarea>
-    </div>
+        <label>Data de nascimento</label>
+        <input ttype="date" name="birthday" value="{{ $client->birthday }}"
+          required />
 
-    <div>
-      <button type="submit">Confirmar edição</button>
-    </div>
-  </form>
+        <label>Endereço</label>
+        <textarea id="address" name="address">{{ $client->address }}</textarea>
+
+        <div class="botoes">
+          <button class="sair" type="button">Sair</button>
+          <button class="editar" type="submit">Editar</button>
+        </div>
+      </form>
+    </section>
+
+    <section class="foto-perfil">
+      <img src="{{ asset('images/foto-usuario.jpg') }}" alt="Foto do usuário" />
+    </section>
+  </main>
   @include('components.footer')
 @endsection
