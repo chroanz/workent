046e8f102e75105720574f05f1935ba621c02f5a
Chroanz <christianhcoa@gmail.com>
2025-05-22 21:34:17 -0300

    feat: implementa botões de paginação no front

--- resources/views/pages/admin/rent/index.blade.php
+++ resources/views/pages/admin/rent/index.blade.php
@@ -49,16 +49,9 @@
         @endforelse
       </tbody>
     </table>
-    <nav aria-label="...">
-      <ul class="pagination pagination-lg">
-        <li class="page-item active" aria-current="page">
-          <span class="page-link">1</span>
-        </li>
-        <li class="page-item"><a class="page-link" href="#">2</a></li>
-        <li class="page-item"><a class="page-link" href="#">3</a></li>
-        <li class="page-item"><a class="page-link" href="#">4</a></li>
-      </ul>
-    </nav>
+    <div class="pagination-container">
+      {{ $rents->links() }}
+    </div>
   </div>
 @endsection
 @section('js')
--- resources/views/pages/admin/room/index.blade.php
+++ resources/views/pages/admin/room/index.blade.php
@@ -48,17 +48,12 @@
         @endforelse
       </tbody>
     </table>
-    <nav aria-label="...">
-      <ul class="pagination pagination-lg">
-        <li class="page-item active" aria-current="page">
-          <span class="page-link">1</span>
-        </li>
-        <li class="page-item"><a class="page-link" href="#">2</a></li>
-        <li class="page-item"><a class="page-link" href="#">3</a></li>
-        <li class="page-item"><a class="page-link" href="#">4</a></li>
-      </ul>
-    </nav>
+    <div class="pagination-container">
+      {{ $rooms->links() }}
+    </div>
   </div>
+  <br>
+  <br>
   @include('components.footer')
 @endsection
 @section('js')
